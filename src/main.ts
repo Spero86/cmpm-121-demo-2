2bbd6b4352aa8848458071fb2d78fec2f4dbcaa1
Patrick James Alcantara <151216151+Spero86@users.noreply.github.com>
2024-11-01 01:14:40 -0700

    Step 1 complete!

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ const APP_NAME = "Pixel Paint Pallete";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
 
 // Step 1 - Initial non-interactive UI layout
 const header = document.createElement("h1");
