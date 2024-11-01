a399f83851dbba55e51329b3368cdf00e94f1731
Patrick James Alcantara <151216151+Spero86@users.noreply.github.com>
2024-11-01 01:12:25 -0700

    Check

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Pixel Paint Pallete";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+// Step 1 - Initial non-interactive UI layout
+const header = document.createElement("h1");
+header.textContent = APP_NAME;
+app.appendChild(header);
+
+const canvas = document.createElement("canvas");
+canvas.id = "canvasPad";
+app.appendChild(canvas);
